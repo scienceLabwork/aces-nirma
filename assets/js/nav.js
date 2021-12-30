751cf8611affc27d9ab551b8f3f4bf51baa18d58
RUDRA SHAH <heysiri876@gmail.com>
2021-12-30 18:47:14 +0530

    new demo

---
+++ assets/js/nav.js
@@ -0,0 +1,23 @@
+(function($) { // Begin jQuery
+    $(function() { // DOM ready
+      
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+      
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      // Toggle open and close nav styles on click
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); // end DOM ready
+  })(jQuery); // end jQuery
